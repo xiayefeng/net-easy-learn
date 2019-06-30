c5aeedf22da5a8ad565ea0bda4cc6ba499f76383
xiayefeng <xiayu_12@yeah.net>
2019-06-30 19:39:01 +0800

    Callbacks入门

---
+++ Callbacks人门/Underscore.js
@@ -0,0 +1,62 @@
+(function(root) {
+	var optionsCache = {};
+	var _ = {
+		callbacks: function(options) {
+			options = typeof options === "string" ? (optionsCache[options] || createOptions(options)) : {};
+            var list = [];
+			var index,length,testting,memory,start,starts;
+			var fire = function(data){
+				memory = options.memory && data;
+				index = starts||0;
+				start = 0;
+				testting = true;  // once 为 true, 保证触发一次
+				length = list.length;
+				for(;index < length;index++){
+					if(list[index].apply(data[0], data[1]) === false && options.stopOnfalse){
+						break;
+					}
+				}
+			}
+			var self = {
+				add: function(){
+					var args = Array.prototype.slice.call(arguments);
+					start = list.length;
+					args.forEach(function(fn){
+						if(toString.call(fn) ==="[object Function]"){
+							list.push(fn);
+						}
+					});
+					if(memory){
+					 starts = start;  // 只有当 memory 为 true 时，才使用下标
+					 fire(memory);	
+					}
+				},
+				fireWith: function(context, arguments){
+					var args = [context, arguments];
+					if(!options.once || !testting){
+					 fire(args);
+					}
+				},
+				fire: function(){
+					self.fireWith(this, arguments);
+				},
+				remove: function(fn) {
+          if(toString.call(fn)==='[object Function]' && list.includes(fn)) {
+						var idx = list.findIndex((item) => item === fn)
+						list.splice(idx, 1)
+					}
+				}
+			}
+			return self;
+		},
+	}
+
+	function createOptions(options) {
+		var object = optionsCache[options] = {};
+		options.split(/\s+/).forEach(function(value) {  // 以空格切割参数
+			object[value] = true;
+		});
+		return object;
+	}
+	root._ = _;
+})(this);
