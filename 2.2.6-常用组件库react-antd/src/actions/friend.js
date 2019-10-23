fc6e674958faf0d524436b4847dadb02fc5b4208
xiayefeng <xiayu_12@yeah.net>
2019-10-23 22:28:04 +0800

    add example

---
+++ 2.2.6-常用组件库react-antd/src/actions/friend.js
@@ -0,0 +1,26 @@
+import { store } from '../store'
+
+export const ADD_FRIEND = 'ADD_FRIEND'
+
+export function addFriend (data) {
+  const dispatch = store.dispatch
+  // dispatch({ type: ADD_FRIEND, data })
+  // request.post("/addFriend",{name}).then(res => {
+  //   dispatch({ type: ADD_FRIEND, data })
+  // })
+  setTimeout(() => {
+    dispatch({ type: ADD_FRIEND, data: { name: 'time-friend', id: 3 } })
+  }, 200)
+}
+
+export function fatchFriend(data) {
+  const dispatch = store.dispatch
+  dispatch({type: 'FETCH_FRIEND', data})
+}
+
+export const DEL_FRIEND = 'DEL_FRIEND'
+
+export function delFriend (id) {
+  const dispatch = store.dispatch
+  dispatch({ type: DEL_FRIEND, data: id })
+}
